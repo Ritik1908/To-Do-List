7f314b7c78d491d7329a49ee2a09b47012a9a497
Ritik1908 <ritikverma8100775754@gmail.com>
2020-04-01 23:57:03 +0530

    Created 1st Template

--- app.js
+++ app.js
@@ -2,19 +2,51 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+// Telling app to use ejs as viewing engine
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extends: true}));
 
 app.get("/", function(req, res){
 	var today = new Date();
 	var currentDay = today.getDay();
-	if(currentDay == 6 || currentDay == 0) {
-		res.send("<h1>Today is sunday.</h1>");
+	var day = "";
+	switch(currentDay) {
+		case 0 :
+			day = "Sunday";
+			break;
+		case 1 :
+			day = "Monday";
+			break;
+		case 2:
+			day = "Tuesday";
+			break;
+		case 3 :
+			day = "Wednesday";
+			break;
+		case 4:
+			day = "Thursday";
+			break;
+		case 5 :
+			day = "Friday";
+			break;
+		case 6:
+			day = "Saturday";
+			break;
+		default:
+		 	console.log("Error"+currentDay);
 	}
-	else {
+	// if(currentDay == 6 || currentDay == 0) {
+	// 	day = "Weekend";
+	// }
+	// else {
+	// 	day = "Weekday";
 		// res.write("<h1>Working Day</h1>");
 		// res.write("<p>It is not weekend.</p>");
-		res.sendFile(__dirname+"/index.html");
-	}
+		// res.sendFile(__dirname+"/index.html");
+	// }
+	res.render("list", {dayOfWeek: day});
 });
 
 app.listen(3000, function() {
